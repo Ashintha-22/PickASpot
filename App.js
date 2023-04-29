5e2be9c393b9a1dd5c9afd33ba3dfcf0c4a51711
Ashintha-22 <deashintha@gmail.com>
2023-04-29 20:03:19 +0530

    init

--- App.js
+++ App.js
@@ -1,13 +1,83 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import 'expo-dev-client';
+import { GoogleSignin, GoogleSigninButton } from '@react-native-google-signin/google-signin';
+import auth from '@react-native-firebase/auth';
+import React, { useState, useEffect } from 'react';
 
 export default function App() {
+  
+  // Set an initializing state whilst Firebase connects
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  GoogleSignin.configure({
+    webClientId: '1017882151115-5d0mcp21oto5gshkau3u6o9cm9dia14o.apps.googleusercontent.com',
+  });
+
+  // Handle user state changes
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  const onGoogleButtonPress = async () => {
+    // Check if your device supports Google Play
+    await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
+    // Get the users ID token
+    const { idToken } = await GoogleSignin.signIn();
+  
+    // Create a Google credential with the token
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+  
+    // Sign-in the user with the credential
+    const user_sign_in = auth().signInWithCredential(googleCredential);
+    user_sign_in.then((user) => {
+      console.log(user);
+    })
+      .catch((error) => {
+        console.log(error);
+    })
+  }
+
+  const signOut = async () => { 
+    try {
+      await GoogleSignin.revokeAccess();
+      await auth().signOut();
+    }
+    catch (error) {
+      console.error(error);
+    }
+  }
+
+
+
+  if (initializing) return null;
+
+  if (!user) {
+    return (
+      <View style={styles.container}>
+        <GoogleSigninButton
+          style={{ width: 300, height: 65, marginTop: 100, borderRadius:50 }}
+          onPress={onGoogleButtonPress}
+        />
+      </View>
+    )
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={{ marginTop: 100, alignItems: 'center' }}>
+        <Text style={styles.text}> Welcome, {user.displayName}</Text>
+      </View>
+      <Button style={{marginTop:300}} title='Sign Out' onPress={signOut}/>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -16,5 +86,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    margin: 10,
   },
 });
